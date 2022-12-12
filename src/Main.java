88528b8f5d1a7f2b409c8401a5bb9acf263cd898
BBaimtoff <120191643+BBaimtoff@users.noreply.github.com>
2022-12-12 16:57:22 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Привет мир!");
+
+        System.out.println("--------------");
+
+        int ageOfMyFather = 37;
+
+        System.out.println("Возраст моего отца: " + ageOfMyFather);
+        System.out.println("Year of birth of my father: " + (2022 - ageOfMyFather));
+
+        ageOfMyFather = 38;
+        System.out.println("Новое перезаписанной значение перемнной: " + ageOfMyFather);
+
+        boolean isMyFatherOlder;
+        int myAge = 15;
+        isMyFatherOlder = ageOfMyFather > myAge;
+
+        System.out.println("Мой отец старше меня ? " + isMyFatherOlder);
+
+        double digit = 15.5;
+        System.out.println(digit + myAge);
+
+        String myCity = "Bishkek";
+
+        System.out.println("My favorite city is " + myCity);
+
+        final boolean isJavaCool = true;
     }
 }
